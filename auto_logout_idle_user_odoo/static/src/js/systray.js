e2a583dd42e53909c16b8d19c7af02ab96cd2741
modfinetech <109356337+modfinetech@users.noreply.github.com>
2025-03-23 17:56:49 +0000

    Update systray.js

--- auto_logout_idle_user_odoo/static/src/js/systray.js
+++ auto_logout_idle_user_odoo/static/src/js/systray.js
@@ -31,33 +31,39 @@ class TimerSystrayItem extends Component{
     passing values of the countdown to the xml
     */
     idle_timer() {
-        var self = this
-        var nowt = new Date().getTime();
-        var date = new Date(nowt);
-        date.setMinutes(date.getMinutes() + self.minutes);
-        var updatedTimestamp = date.getTime();
-        /** Running the count down using setInterval function */
-        var idle = setInterval(function() {
-            var now = new Date().getTime();
-            var distance = updatedTimestamp - now;
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var self = this
+    var nowt = new Date().getTime();
+    var date = new Date(nowt);
+    date.setMinutes(date.getMinutes() + self.minutes);
+    var updatedTimestamp = date.getTime();
+    /** Running the count down using setInterval function */
+    var idle = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = updatedTimestamp - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        // Ajout cette vérification
+        const timerElement = document.querySelector("#idle_timer");
+        if (timerElement) {
             if (hours && days) {
-                self.state.idle_time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+                timerElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
             } else if (hours) {
-                self.state.idle_time = hours + "h " + minutes + "m " + seconds + "s ";
+                timerElement.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
             } else {
-                self.state.idle_time = minutes + "m " + seconds + "s ";
+                timerElement.innerHTML = minutes + "m " + seconds + "s ";
             }
+            
             /** if the countdown is zero the link is redirect to the login page*/
             if (distance < 0) {
                 clearInterval(idle);
-                self.state.idle_time = "EXPIRED";
+                timerElement.innerHTML = "EXPIRED";
                 location.replace("/web/session/logout")
             }
-        }, 1000);
+        }
+    }, 1000);
         /**
         checking if the onmouse-move event is occur
         */
